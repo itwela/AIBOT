7e456ebf2dc3375d9cec94b5458986f1b39df71d
itwela <deanandnostrand@gmail.com>
2023-03-26 10:30:57 -0400

    got javascript finally working

--- Files/index.js
+++ Files/index.js
@@ -1,3 +1,11 @@
 // javascript
 
-d3.select('h1').style('color', 'red');
+d3.select('h1').style('color', 'red')
+.attr('class', 'heading')
+.text('Update h1 tag')
+
+d3.select('body').append('p').text('First paragraph')
+d3.select('body').append('p').text('Second paragraph')
+d3.select('body').append('p').text('Third paragraph')
+
+d3.selectAll('p').style('color', 'green')
